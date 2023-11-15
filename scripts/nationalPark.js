43a6f632795d72868a0fae164ad19ea5aaed63dc
Juan11211 <avilesjuan628@gmail.com>
2023-11-15 00:10:21 -0500

    working on styling

--- scripts/nationalPark.js
+++ scripts/nationalPark.js
@@ -1,6 +1,8 @@
 "use strict";
 
-window.onload = function () {
+window.onload = function (e) {
+    e.preventDefault(); 
+
     let searchByLocation = document.getElementById("searchByLocation");
     let searchByParkType = document.getElementById("searchByParkType");
     let locationDropDown = document.getElementById("locationDropDown");
@@ -19,21 +21,29 @@ window.onload = function () {
 
 };
 
+function clearDisplayedParks() {
+    let displayParkLocation = document.getElementById("displayParkLocation");
+    displayParkLocation.innerHTML = "";
+    displayParkLocation.style.display = "none";
+}
+
 function searchByLocationHandler() {
     if (searchByLocation.checked) {
-        searchByParkType.checked = false; // Uncheck the other radio button
+        searchByParkType.checked = false;
         parkTypeDropDown.style.display = "none";
         locationDropDown.style.display = "block";
         populateLocationDropDown();
+        clearDisplayedParks(); // Clear displayed parks when changing the selection
     }
 }
 
 function searchByParkTypeHandler() {
     if (searchByParkType.checked) {
-        searchByLocation.checked = false; // Uncheck the other radio button
+        searchByLocation.checked = false;
         locationDropDown.style.display = "none";
         parkTypeDropDown.style.display = "block";
         populateParkTypeDropDown();
+        clearDisplayedParks(); // Clear displayed parks when changing the selection
     }
 }
 
@@ -82,7 +92,7 @@ function parkTypeDropDownValue() {
 }
 
 function displayAllParks() {
-    // Display all parks without filtering
+    //Display all parks without filtering
     displayLocation(nationalParksArray);
 }
 
@@ -94,30 +104,15 @@ function displayLocation(selectedParks) {
     if (selectedParks.length > 0) {
         displayParkLocation.style.display = "block";
 
-        // selectedParks.forEach(park => {
-        //     const parkCard = document.createElement('div');
-        //     parkCard.classList.add('card');
-            
-
-        //     parkCard.innerHTML = `
-        //         <div class="card-body">
-        //             <h5 class="card-title">${park.LocationName}</h5>
-        //             <p class="card-text">Location: ${park.City}, ${park.State}</p>
-        //         </div>
-        //     `;
-
-        //     displayParkLocation.appendChild(parkCard);
-        // });
-
         selectedParks.forEach(park => {
             const parkCard = document.createElement('div');
-            parkCard.classList.add('card');                 
+            parkCard.classList.add('card', 'mb-3');  // Adding Bootstrap card class and margin-bottom
         
             const cardBody = document.createElement('div');
-            cardBody.classList.add('card-body');
-        
+            cardBody.classList.add('card-body', 'd-flex', 'justify-content-between'); // Added flex utility classes
+
             const cardTitle = document.createElement('h5');
-            cardTitle.classList.add('card-title');
+            cardTitle.classList.add('card-title', 'text-dark');  // Adding Bootstrap text color class
             const titleText = document.createTextNode(park.LocationName);
             cardTitle.appendChild(titleText);
         
@@ -125,16 +120,55 @@ function displayLocation(selectedParks) {
             cardText.classList.add('card-text');
             const locationText = document.createTextNode(`Location: ${park.City}, ${park.State}`);
             cardText.appendChild(locationText);
+
+            // Check if the park has a Visit link
+            if (park.Visit) {
+                const parkLink = document.createElement('a');
+                parkLink.setAttribute('href', park.Visit);
+                parkLink.setAttribute('target', '_blank');
+                parkLink.classList.add('btn', 'btn-dark', 'btn-sm', 'text-center'); 
+                parkLink.innerHTML = 'Visit Property';
+                cardBody.appendChild(parkLink);
+            }
         
             cardBody.appendChild(cardTitle);
             cardBody.appendChild(cardText);
-        
             parkCard.appendChild(cardBody);
         
             displayParkLocation.appendChild(parkCard);
         });
-        
-        
     }
 }
+
+// function displayLocation(selectedParks) {
+//     let displayParkLocation = document.getElementById("displayParkLocation");
+
+//     displayParkLocation.innerHTML = "";
+
+//     if (selectedParks.length > 0) {
+//         displayParkLocation.style.display = "block";
+
+//         selectedParks.forEach(park => {
+//             const parkCard = `
+//             <div class="container row>
+//             <div class="card mb-3">
+//             <div class="card-body">
+//                 <h5 class="card-title text-primary">${park.LocationName}</h5>
+//                 <p class="card-text">Location: ${park.City}, ${park.State}</p>
+//                 ${park.Visit ? `<a href="${park.Visit}" class="btn btn-dark btn-sm" target="_blank">Visit Property</a>` : ''}
+//             </div>
+//         </div>;
+//     </div>
+            
+//         `
+               
+
+//             displayParkLocation.innerHTML += parkCard;
+//         });
+//     }
+// }
+
+
+
+
 
